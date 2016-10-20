cfa7ef02c4754c0b65effbbb4b225402f0335e79
Andreas Philippi <anphil@users.noreply.github.com>
2016-10-20 08:44:03 +0200

    Cleanup for GitHub Pages

--- src/js/models/gratitudeItem.js
+++
@@ -1,7 +0,0 @@
-var GratitudeItem = function()
-{
-  this.text = '';
-  this.count = 1;
-  this.timestamp = Date.now();
-  this.category = '';
-};
--- src/js/models/gratitudePrompt.js
+++
@@ -1,7 +0,0 @@
-var GratitudePrompt = function(text, category, topCategoryName, bottomCategoryName)
-{
-  this.text = text;
-  this.category = category;
-  this.topCategoryName = topCategoryName;
-  this.bottomCategoryName = bottomCategoryName;
-};
